c5527336d301f36bfc8b9a3acb1035dec195e5a5
igor <igor199917>
2022-10-25 19:09:36 +0300

    test

--- helpers/candyMachineInfo.js
+++ helpers/candyMachineInfo.js
@@ -1,10 +1,10 @@
-export const candyMachineAddress = "0x3b3a0901cfc91b289cd16b62b28fd850e52a2921d463945a3722c33a81bad453";
-export const collectionName = "TestCollection1232dodo"; // Case sensitive!
+export const candyMachineAddress = "0xa4e34c62b207c229105eb78384f3b370fc10b4e4d7b8a42db4fcf6bd842b62d7";
+export const collectionName = "TestCollection32323332dodo"; // Case sensitive!
 export const collectionCoverUrl = "https://cloudflare-ipfs.com/ipfs/QmeCuKGLvAGrBAPGdaRh2hkTjXsXuk8VEUHNoYRD4cJG51";
 export const mode = "mainnet"; // "dev" or "test" or "mainnet"
 
 export let NODE_URL;
-export const CONTRACT_ADDRESS = "0xdf5c814388f4162f353e14f6123fcba8f39a958e4a2640e38e9e2c7cdfd2ac1d";
+export const CONTRACT_ADDRESS = "0xc071ef709539f7f9372f16050bf984fe6f11850594b8394f11bc74d22f48836b";
 export const COLLECTION_SIZE = 10
 let FAUCET_URL;
 if (mode == "dev") {
@@ -17,3 +17,4 @@ if (mode == "dev") {
     NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
     FAUCET_URL = null;
 }
+
