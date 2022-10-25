a56cc3b558e049c8273f44b159333a5fb97331a9
Soulwellnft <116663129+Soulwellnft@users.noreply.github.com>
2022-10-25 20:08:57 +0300

    Update candyMachineInfo.js

--- helpers/candyMachineInfo.js
+++ helpers/candyMachineInfo.js
@@ -1,11 +1,11 @@
 export const candyMachineAddress = "0xb0c25580975b1ba425a95c309e0a44b912cfd32331c1ce451959e88edf357850";
 export const collectionName = "Aptos Croak"; // Case sensitive!
-export const collectionCoverUrl = "https://cdn.discordapp.com/attachments/1034442838966026261/1034487062910468217/unknown.png";
+export const collectionCoverUrl = "https://gateway.pinata.cloud/ipfs/QmXA15EJwSsKpHKYTuxWFyTRr1iaJnoMFUzFbeaFhj5sGi";
 export const mode = "mainnet"; // "dev" or "test" or "mainnet"
 
 export let NODE_URL;
 export const CONTRACT_ADDRESS = "0xc071ef709539f7f9372f16050bf984fe6f11850594b8394f11bc74d22f48836b";
-export const COLLECTION_SIZE = 10
+export const COLLECTION_SIZE = 666
 let FAUCET_URL;
 if (mode == "dev") {
     NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
